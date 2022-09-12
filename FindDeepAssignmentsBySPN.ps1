515921ddc2be7f22cc44b0572b0d4f89337ae176
Zeeshan <zmustafa@microsoft.com>
2022-09-12 15:14:41 -0500

    Update FindDeepAssignmentsBySPN.ps1

--- FindDeepAssignmentsBySPN.ps1
+++ FindDeepAssignmentsBySPN.ps1
@@ -1,13 +1,38 @@
-Select-AzSubscription -SubscriptionName "<SUBSCRIPTION_NAME>"
-$spn=@("<SPN1>","<SPN2...>")
+$searchForRbac = @("Contributor", "Owner")
+$searchUserTypes = @("User") ##$searchUserTypes = @("Group","User", "ServicePrincipal")
 
-$Resources = Get-AzResource 
-foreach ($Resource in $Resources) 
-{
-    $ass = Get-AzRoleAssignment -Scope $Resource.Id | Where-Object {$_.DisplayName -in $spn}
-    foreach ($ax in $ass) 
-    {
-        Write-Host $ax.DisplayName "`t" $ax.Scope "`t" $ax.RoleDefinitionName
+$subNames = @("Subscription Name 1",
+    "Subscription Name 2")
+
+foreach ($subName in $subNames) {
+    $azSub = Get-AzSubscription -SubscriptionName $subName
+    Set-AzContext $azSub.id | Out-Null
+    Write-Host "Scanning subscription : "  $subName
+    Write-Host "------------------------------------------------------------------"
+    $azlogs = @()
+    $azlogs  += @($(("Scope`tDisplayName`tRBAC`tUserType")))
+    $foundassigns = @()
+
+    $Resources = Get-AzResource  
+    foreach ($r in $Resources) {
+       
+        $ResourceId = $r.ResourceId
+ 
+        $assign = Get-AzRoleAssignment -Scope $ResourceId -WarningAction Ignore | Where-Object { $_.RoleDefinitionName -in $searchForRbac } `
+        | Where-Object { $_.ObjectType -in $searchUserTypes } 
+
+        foreach ($a in $assign) {
+            if ( $foundassigns -notcontains $($a.Scope + "`t" + $a.DisplayName + "`t" + $a.RoleDefinitionName + "`t" + $a.ObjectType)) {
+                $v = @($(("" + $a.Scope + "`t" + $a.DisplayName + "`t" + $a.RoleDefinitionName + "`t" + $a.ObjectType )))
+                $foundassigns += @($($a.Scope + "`t" + $a.DisplayName + "`t" + $a.RoleDefinitionName + "`t" + $a.ObjectType))
+                $azlogs += $v
+                Write-Host  $v
+            }
+        }
     }
-     
+
+    $azSubName = $azSub.Name
+    $azlogs >> .\$azSubName.csv
+    
 }
+ 
